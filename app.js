79227fae3314bb288f8b1042696bb97467e3c861
karasikrus <karasikrus@gmail.com>
2020-03-05 18:15:45 +0300

    quest functions displaySlots function

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const CoreController = require('./controller/core-controller');
 
-console.log(countNumbers(CoreController.getSpinResult().matrix));
-console.log(checkWin(countNumbers(CoreController.getSpinResult().matrix)))
+let slotResult = CoreController.getSpinResult();
+let slotMaps = countNumbers(slotResult.matrix);
+
+console.log(slotResult.matrix);
+displaySlots(slotResult.matrix);
+console.log(slotMaps);
+console.log('win = ', checkWin(slotMaps));
+console.log('spins = ', spinsQuest(0));
+console.log('money spent = ', moneyQuest(slotResult.spentMoney, 0));
+console.log('combos got = ', comboRowQuest(slotMaps, 0));
+console.log('symbol got = ', symbolQuest(slotMaps));
+
+function displaySlots(matrix) {
+    const rows = 3;
+    let strings = [];
+    for(let i = 0; i< rows; i++){
+        strings[i] = [];
+    }
+    for(let i = 0; i< matrix.length; i++){
+        strings[i%rows].push(matrix[i]);
+    }
+    for(let i = 0; i< rows; i++){
+        console.log(strings[i].join(' '));
+    }
+}
 
 function countNumbers(matrix) {
     const rows = 3;
@@ -28,4 +51,30 @@ function checkWin(numberMaps) {
         }
     }
     return false;
+}
+function spinsQuest(previousValue){
+    return previousValue+1;
+}
+function moneyQuest(money, totalSpent){
+    return totalSpent+money;
+}
+function comboRowQuest(numberMaps, previousValue) {
+    for (const row of numberMaps){
+        for (const v of row.values()){
+            if(v>2){
+                return previousValue+1;
+            }
+        }
+    }
+    return previousValue;
+}
+function symbolQuest(numberMaps) {
+    for (const row of numberMaps){
+        for (const k of row.keys()){
+            if(k===71){
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
